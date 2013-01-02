3bd02f4b0b4723c3a3660d5a4ef2a345db441dd8
Alexander Skjolden <alex@plastboks.net>
2013-01-02 09:51:56 +0100

    untested confirmation example

--- software/desktop.sh
+++ software/desktop.sh
@@ -1,43 +1,94 @@
-# THIS FILE IS ONLY FOR REFERANCE!!!  
+# THIS FILE IS ONLY FOR REFERANCE!!!
+
+
 # update
-sudo pacman -Syu
+read -p "Update repos before installing ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -Syu
+fi
 
 
 # xserver and some other
-sudo pacman -S \
-  xorg-server \
-  xorg-xinit \
-  xorg-xrdb \
-  xorg-xrandr \
-  xf86-video-nouveau \
-  xautolock \
-  xorg-utils \
-  xorg-apps \
-  i3 \
-  rxvt-unicode \
-  dmenu \
-  
+read -p "Install xorg and i3 ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    xorg-server \
+    xorg-xinit \
+    xorg-xrdb \
+    xorg-xrandr \
+    xf86-video-nouveau \
+    xautolock \
+    xorg-utils \
+    xorg-apps \
+    i3 \
+    rxvt-unicode \
+    dmenu \
+fi
+
 
 # fonts
+read -p "Install fonts ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
 sudo pacman -S \
   ttf-linux-libertine \
   ttf-freefont \
   ttf-liberation 
+fi
+
 
 # sound
+read -p "Install soundutils and player ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
 sudo pacman -S \
   alsa-utils \
   moc
+fi
+
 
 # video
+read -p "Install movieutils and player ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
 sudo pacman -S \
   mplayer
+fi
+
+
+# editors 
+read -p "Install editors ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    vim
+fi
+
+
+# network tools
+read -p "Install various network tools ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    wget \
+    nfs-utils \
+fi
+
+
+# network time tools
+read -p "Install various network tools ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \ 
+    ntp
+fi
+
 
 # sensors
+read -p "Install hardwaresensors utils ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
 sudo pacman -S \ 
   lm_sensors
+fi
+
 
 # ssh and rsync
+read -p "Install ssh, encryption and sync utils ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
 sudo pacman -S \
   openssh \
   sshfs \
@@ -45,59 +96,99 @@ sudo pacman -S \
   rsync \
   keychain \
   ecryptfs-utils \
-  encfs
+  encfs \
+  openssl \
+  zlib \
+fi
 
-# mail
-sudo pacman -S mutt
 
-# chat
-sudo pacman -S finch
+# mail, chat, rss
+read -p "Install mail, chat and rss utils ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+sudo pacman -S \
+  mutt \
+  finch \
+  newsbeuter
+fi
+
 
 # web 
+read -p "Install webbrowsers ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
 sudo pacman -S \
   luakit \
-#  dwb \
+  dwb \
+  firefox
+fi
+
+
+# java browser plugin
+read -p "Install java browser plugin ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    icedtea-web
+fi
+
+# flash browser plugin
+read -p "Install flash browser plugin ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    flashplugin
+fi
 
 # smartcard 
+read -p "Install smartcard utils ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
 sudo pacman -S \
   pcsc-tools \
   ccid 
+fi
 
 
 # file browsers and other tools
-#sudo pacman -S \
-#  pcmanfm \
-#  gamin \
-#  tangerine-icon-theme \
-#  gnome-menus \
-#  gvfs \
-#  gvfs-smb \
-#  gvfs-afp \
-#  gvfs-obexftp \
-#  gtk3 \
-#  librsvg \
-#  libsvg \
-#  imagemagick \
-#  ghostscript \
-#  evince \
-#  libreoffice
+read -p "Install graphical file browsers and other various tools ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    pcmanfm \
+    gamin \
+    tangerine-icon-theme \
+    gnome-menus \
+    gvfs \
+    gvfs-smb \
+    gvfs-afp \
+    gvfs-obexftp \
+    gtk3 \
+    librsvg \
+    libsvg \
+    imagemagick \
+    ghostscript \
+    evince 
+fi
+
+
+# libreoffice
+read -p "Install libreoffice ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    libreoffice
+fi
 
 
 # notification
-sudo pacman -S \
-  dunst \
-  inotify-tools
-
+read -p "Install notification tools ? " -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    dunst \
+    inotify-tools
+fi
 
 # other
-sudo pacman -S \
-  cclive \
-  quvi \
-  openssl \
-  zlib \
-  wget \
-  git \
-  vim \
-  nfs-utils \
-  htop \
-  tmux \
+read -p "Install other various cli tools ?" -n 1 -r
+if [[ $REPLY =~[Yy]$ ]]; then
+  sudo pacman -S \
+    cclive \
+    quvi \
+    git \
+    htop \
+    tmux \
+fi
