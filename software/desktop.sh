4b6083e52db8466adec08c19edc29473a0636fcf
Alexander Skjolden <alex@plastboks.net>
2013-01-04 11:06:47 +0100

    tested and modified the desktop install script

--- software/desktop.sh
+++ software/desktop.sh
@@ -2,15 +2,18 @@
 
 
 # update
-read -p "Update repos before installing ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+read -p "Update repos before installing [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -Syu
 fi
 
 
 # xserver and some other
-read -p "Install xorg and i3 ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install xorg and i3 [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     xorg-server \
     xorg-xinit \
@@ -22,142 +25,170 @@ if [[ $REPLY =~[Yy]$ ]]; then
     xorg-apps \
     i3 \
     rxvt-unicode \
-    dmenu \
+    dmenu 
 fi
 
 
 # fonts
-read -p "Install fonts ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \
-  ttf-linux-libertine \
-  ttf-freefont \
-  ttf-liberation 
+printf "\n"
+read -p "Install fonts [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \
+    ttf-linux-libertine \
+    ttf-freefont \
+    ttf-liberation 
 fi
 
 
 # sound
-read -p "Install soundutils and player ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \
-  alsa-utils \
-  moc
+printf "\n"
+read -p "Install soundutils and player [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \
+    alsa-utils \
+    moc
 fi
 
 
 # video
-read -p "Install movieutils and player ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \
-  mplayer
+printf "\n"
+read -p "Install movieutils and player [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \
+    mplayer
 fi
 
 
 # editors 
-read -p "Install editors ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install editors [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     vim
 fi
 
 
 # network tools
-read -p "Install various network tools ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install various network tools [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     wget \
-    nfs-utils \
+    nfs-utils 
 fi
 
 
 # network time tools
-read -p "Install various network tools ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install various networktime tools [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \ 
     ntp
   CRONDATE="/etc/cron.hourly/ntpdate"
   if [[ -f $CRONDATE ]]; then
-    echo "${CRONDATE} already exists"
+    printf "${CRONDATE} already exists"
   else
-    echo "Making ${CRONDATE} and population it with data."
+    printf "Making ${CRONDATE} and population it with data."
     sudo touch $CRONDATE
     sudo chmod +x $CRONDATE
-    echo "#!/bin/sh" > $CRONDATE
-    echo "/usr/bin/ntpdate pool.ntp.org" >> $CRONDATE
+    printf "#!/bin/sh" > $CRONDATE
+    printf "/usr/bin/ntpdate pool.ntp.org" >> $CRONDATE
   fi
 fi
 
 
 # sensors
-read -p "Install hardwaresensors utils ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \ 
-  lm_sensors
+printf "\n"
+read -p "Install hardwaresensors utils [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \ 
+    lm_sensors
 fi
 
 
 # ssh and rsync
-read -p "Install ssh, encryption and sync utils ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \
-  openssh \
-  sshfs \
-  unison \
-  rsync \
-  keychain \
-  ecryptfs-utils \
-  encfs \
-  openssl \
-  zlib \
+printf "\n"
+read -p "Install ssh, encryption and sync utils [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \
+    openssh \
+    sshfs \
+    unison \
+    rsync \
+    keychain \
+    ecryptfs-utils \
+    encfs \
+    openssl \
+    zlib 
 fi
 
 
 # mail, chat, rss
-read -p "Install mail, chat and rss utils ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \
-  mutt \
-  finch \
-  newsbeuter
+printf "\n"
+read -p "Install mail, chat and rss utils [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \
+    mutt \
+    finch \
+    newsbeuter
 fi
 
 
 # web 
-read -p "Install webbrowsers ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \
-  luakit \
-  dwb \
-  firefox
+printf "\n"
+read -p "Install webbrowsers [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \
+    luakit \
+    dwb \
+    firefox
 fi
 
 
 # java browser plugin
-read -p "Install java browser plugin ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install java browser plugin [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     icedtea-web
 fi
 
 # flash browser plugin
-read -p "Install flash browser plugin ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install flash browser plugin [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     flashplugin
 fi
 
 # smartcard 
-read -p "Install smartcard utils ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
-sudo pacman -S \
-  pcsc-tools \
-  ccid 
+printf "\n"
+read -p "Install smartcard utils [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
+  sudo pacman -S \
+    pcsc-tools \
+    ccid 
 fi
 
 
 # file browsers and other tools
-read -p "Install graphical file browsers and other various tools ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install graphical file browsers and other various tools [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     pcmanfm \
     gamin \
@@ -169,7 +200,7 @@ if [[ $REPLY =~[Yy]$ ]]; then
     gvfs-obexftp \
     gtk3 \
     librsvg \
-    libsvg \
+    libpng \
     imagemagick \
     ghostscript \
     evince 
@@ -177,28 +208,36 @@ fi
 
 
 # libreoffice
-read -p "Install libreoffice ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install libreoffice [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     libreoffice
 fi
 
 
 # notification
-read -p "Install notification tools ? " -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install notification tools [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     dunst \
     inotify-tools
 fi
 
 # other
-read -p "Install other various cli tools ?" -n 1 -r
-if [[ $REPLY =~[Yy]$ ]]; then
+printf "\n"
+read -p "Install other various cli tools [y/N]" -n 1 -r
+if [[ $REPLY =~ [Yy]$ ]]; then
+  printf "\n"
   sudo pacman -S \
     cclive \
     quvi \
     git \
     htop \
-    tmux \
+    tmux 
 fi
+
+printf "\n"
